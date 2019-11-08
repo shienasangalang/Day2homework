91aa0cac1c9ab66691b606d01e0bac9b7f9c5595
shienasangalang <57032005+shienasangalang@users.noreply.github.com>
2019-11-08 22:11:39 +0800

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,16 @@
+# If Statements and Comments Exercise
+
+# Make an if-else statement if year is between 2000 and 2100
+# (including both numbers), then print out:
+# "Welcome to the 21st century"
+# Else print out:
+# "You are before or after the 21st century"
+
+year = 1830
+
+#If year is between YYYY and YYYY
+if year >= 2000 and year <= 2100:
+    print("Welcome to the 21st century")
+else:
+    print("You are before or after the 21st century")
+
---
+++ exercise4.py
@@ -0,0 +1,9 @@
+# Challenge - Lists Exercise
+
+# Create a variable named shoes that is a
+# list of the following items, in order:
+
+shoes = ["Spizikes","Air Force 1","Curry 2","Melo 5"]
+
+for shoe in shoes:
+    print(shoe)
